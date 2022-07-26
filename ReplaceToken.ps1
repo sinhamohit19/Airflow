75e66d47416618f33b67485493d8fcd42981f7f1
sinhamohit19 <87541234+sinhamohit19@users.noreply.github.com>
2022-07-26 14:26:10 +0530

    Add files via upload

---
+++ ReplaceToken.ps1
@@ -0,0 +1,3 @@
+﻿$content= Get-Content -Path 'C:\Users\sinha\Downloads\Input.txt'
+$UpdateFile=$content.Replace('morning','afternoon')
+$UpdateFile | Set-Content -Path 'C:\Users\sinha\Downloads\Output.txt'
